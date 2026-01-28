16c55a70d6ca63eed96cd99f657c44d8a7166e9d
sinanalizz123-max <sinanalizz123@gmail.com>
2026-01-28 13:20:57 +0530

    UI: Settings dark/light mode + theme colors (Figma match)

--- app/src/main/java/com/netmonitor/app/SettingsFragment.kt
+++ app/src/main/java/com/netmonitor/app/SettingsFragment.kt
@@ -4,25 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
-import com.netmonitor.app.databinding.FragSettingsBinding
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class SettingsFragment : Fragment() {
-
-    private var _binding: FragSettingsBinding? = null
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragSettingsBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+        val v = inflater.inflate(R.layout.frag_settings, container, false)
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        val darkToggle = v.findViewById<SwitchMaterial>(R.id.switchDarkMode)
+
+        darkToggle.isChecked =
+            AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+
+        darkToggle.setOnCheckedChangeListener { _, isChecked ->
+            AppCompatDelegate.setDefaultNightMode(
+                if (isChecked)
+                    AppCompatDelegate.MODE_NIGHT_YES
+                else
+                    AppCompatDelegate.MODE_NIGHT_NO
+            )
+        }
+
+        return v
     }
 }
