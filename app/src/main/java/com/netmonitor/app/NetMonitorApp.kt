37743f570af020a05b4b689da89f861b6e6cd646
sinanalizz123-max <sinanalizz123@gmail.com>
2026-01-28 18:30:40 +0530

    fix: add global crash logger to write crash.log

--- app/src/main/java/com/netmonitor/app/NetMonitorApp.kt
+++ app/src/main/java/com/netmonitor/app/NetMonitorApp.kt
@@ -1,11 +1,26 @@
 package com.netmonitor.app
 
 import android.app.Application
-import com.netmonitor.app.util.CrashLogger
+import android.util.Log
+import java.io.File
+import java.io.FileWriter
+import java.io.PrintWriter
 
 class NetMonitorApp : Application() {
+
     override fun onCreate() {
         super.onCreate()
-        CrashLogger.init(this)
+
+        Thread.setDefaultUncaughtExceptionHandler { _, e ->
+            try {
+                val file = File(getExternalFilesDir(null), "crash.log")
+                PrintWriter(FileWriter(file, true)).use {
+                    it.println("---- CRASH ----")
+                    e.printStackTrace(it)
+                }
+            } catch (t: Throwable) {
+                Log.e("CrashLogger", "Failed to write crash", t)
+            }
+        }
     }
 }
